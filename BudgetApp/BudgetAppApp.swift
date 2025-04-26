f94115f0296891f3896e52dad707c9d28491e7a6
Linas Nutautas <info@linasdev.io>
2025-04-27 00:41:35 +0200

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Linas on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
